59cddd9dd94a405f3ab465caaa2881522b85c27a
Maxence Ho <maxence.ho@i-bp.fr>
2019-02-24 18:44:23 +0100

    Misc - Reorganize files structure.

---
+++ currency_converter/currency_converter/Application/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  currency_converter
+//
+//  Created by HO Maxence (i-BP) on 22/02/2019.
+//  Copyright © 2019 chiminhTT. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+}
+

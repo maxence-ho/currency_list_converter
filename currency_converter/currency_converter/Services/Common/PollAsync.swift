3bb3a182cd4128703e4135c7e2e9185fed90a9f7
Maxence Ho <maxence.ho@i-bp.fr>
2019-02-26 01:47:41 +0100

    Feature - service_provider - Add Polling support + tests.

---
+++ currency_converter/currency_converter/Services/Common/PollAsync.swift
@@ -0,0 +1,65 @@
+//
+//  PollingAsync.swift
+//  currency_converter
+//
+//  Created by HO Maxence (i-BP) on 24/02/2019.
+//  Copyright © 2019 chiminhTT. All rights reserved.
+//
+
+import Foundation
+import Promises
+
+let concurrentPollingQueue = DispatchQueue(label: "polling_queue", attributes: .concurrent)
+
+/** Defines the structure */
+class PollAsync<T: Codable>
+{
+    /** Factory generating the request's promises */
+    let requestFactory: RequestFactory<T>!
+    /** Completion handler to be called when the request's promise succeeds */
+    let completion: ((T) -> Void)!
+    /** Polling interval */
+    let interval: TimeInterval!
+    /** Polling timer */
+    let timer = DispatchSource.makeTimerSource(queue: concurrentPollingQueue)
+    
+    init(requestFactory: RequestFactory<T>,
+         completion: @escaping ((T) -> Void),
+         interval: TimeInterval )
+    {
+        self.requestFactory = requestFactory
+        self.completion = completion
+        self.interval = interval
+    }
+    
+    deinit
+    {
+        timer.setEventHandler {}
+        timer.cancel()
+    }
+}
+
+extension PollAsync
+{
+    /** Setup and starts the polling timer and its handler (based on the `completion` parameter) */
+    func start()
+    {
+        timer.schedule(
+            deadline: .now(),
+            repeating: .milliseconds(Int(interval * 1000)),
+            leeway: .milliseconds(100)
+        )
+        
+        timer.setEventHandler { [weak self] in
+            guard let self = self else { return }
+            self.requestFactory
+                .getRequest()
+                .then({ [weak self] response in
+                    guard let self = self else { return }
+                    self.completion(response)
+                })
+        }
+        
+        timer.resume()
+    }
+}
--- currency_converter/currency_converter/Services/Providers/CurrencyServiceProvider.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CurrencyServiceProvider.swift
-//  currency_converter
-//
-//  Created by HO Maxence (i-BP) on 24/02/2019.
-//  Copyright © 2019 chiminhTT. All rights reserved.
-//
-
-import Foundation

f550cee0b4f6f48386e2fa1558201edcd21e6531
Maxence Ho <maxence.ho@i-bp.fr>
2019-02-26 01:45:03 +0100

    Feature - service_provider - Add Currency model tests

---
+++ currency_converter/currency_converterTests/Services/Models/CurrencyRates.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyRates.swift
+//  currency_converterTests
+//
+//  Created by HO Maxence (i-BP) on 26/02/2019.
+//  Copyright © 2019 chiminhTT. All rights reserved.
+//
+
+import XCTest
+
+@testable import currency_converter
+
+class CurrencyRatesTests: XCTestCase
+{
+    func test_serialization()
+    {
+        guard
+            let path = Bundle(for: CurrencyRatesTests.self).path(forResource: "test_getCurrencyRates_EUR", ofType: "json"),
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path),
+                                 options: .mappedIfSafe),
+            let currencyRates = try? JSONDecoder().decode(CurrencyRates.self,
+                                                          from: data)
+        else
+        {
+            XCTFail()
+            return
+        }
+
+        XCTAssert(currencyRates.base == "EUR")
+        XCTAssert(currencyRates.rates.count == 32)
+    }
+}
+

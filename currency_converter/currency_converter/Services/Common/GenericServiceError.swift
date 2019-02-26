a3dfd409f85e97b80070c6f435c5802c6748df05
Maxence Ho <maxence.ho@i-bp.fr>
2019-02-26 01:46:51 +0100

    Feature - service_provider - Add CurrencyServiceProvider

---
+++ currency_converter/currency_converter/Services/Common/GenericServiceError.swift
@@ -0,0 +1,12 @@
+//
+//  GenericServiceError.swift
+//  currency_converter
+//
+//  Created by HO Maxence (i-BP) on 26/02/2019.
+//  Copyright © 2019 chiminhTT. All rights reserved.
+//
+
+enum GenericServiceError: Error
+{
+    case responseWithoutData
+}

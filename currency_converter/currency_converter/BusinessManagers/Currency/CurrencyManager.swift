3348f51803d6a8f819a600adb9be1ed7fc297ff2
Maxence Ho <maxence.ho@i-bp.fr>
2019-03-02 00:04:03 +0100

    Feature - manager - Add Currency Manager

---
+++ currency_converter/currency_converter/BusinessManagers/Currency/CurrencyManager.swift
@@ -0,0 +1,92 @@
+//
+//  CurrencyManager.swift
+//  currency_converter
+//
+//  Created by HO Maxence (i-BP) on 01/03/2019.
+//  Copyright © 2019 chiminhTT. All rights reserved.
+//
+
+/** Protocol used to listen to CurrencyManager and act upon notification - delegate pattern */
+protocol CurrencyManagerListener: class {
+    /** Delegate method to implement to manage notifications */
+    func currencyRatesDidChange(newCurrencyRates: [AugmentedCurrencyRateBO])
+}
+
+/** Class that can continuously fetch AugmentedCurrencyRates relative to a base
+ * currency. Classes can implement the delegate protocol above to be notified
+ * when the currency rates are updated.
+ */
+class CurrencyManager
+{
+    /** List of currency information that is fetched from a mocked service - embedded json file */
+    private let currencyInfoDict: Dictionary<CurrencyCode, CurrencyInfo>
+    
+    /** Polling task used by the manager to fetch CurrencyRates periodically */
+    private var pollTask: PollAsyncTask<CurrencyRates>?
+    
+    /** List of listeners that will be notified when `currencyRates` changes */
+    weak var delegate: CurrencyManagerListener?
+    
+    /** Base currency used that acts as reference for the conversion rates */
+    var baseCurrency: CurrencyCode {
+        /** Upon changing the base currency, we replace the polling task by one
+         * requesting data with the new base currency
+         */
+        didSet {
+            self.pollTask = nil
+            let requestFactory = RequestFactory(
+                getRequest: { return CurrencyServiceProvider.getCurrencyRates(baseCurrency: self.baseCurrency) }
+            )
+            self.configurePolling(withRequestFactory: requestFactory)
+        }
+    }
+    
+    /** Up to date currency rates based on the base currency */
+    private var currencyRates: [AugmentedCurrencyRateBO] = [] {
+        /** Upon changing the currencyRates, we notify the delegate that this
+         * value has changed and pass the new value
+         */
+        didSet {
+            delegate?.currencyRatesDidChange(newCurrencyRates: self.currencyRates)
+        }
+    }
+
+    /** Init - failable.
+     * parameter baseCurrency: code of the base currency
+     */
+    init?(baseCurrency: CurrencyCode)
+    {
+        guard
+            let fetchedCurrencyInfoDict = try? CurrencyServiceProvider.getCurrencyListInfo()
+        else { return nil}
+        
+        self.baseCurrency = baseCurrency
+        /** Fetches the currency info list from the mocked service */
+        self.currencyInfoDict = fetchedCurrencyInfoDict
+    }
+}
+
+extension CurrencyManager
+{
+    /** Function that create and activate the manager's polling task */
+    func startPolling()
+    {
+        let requestFactory = RequestFactory(
+            getRequest: { return CurrencyServiceProvider.getCurrencyRates(baseCurrency: self.baseCurrency) }
+        )
+        configurePolling(withRequestFactory: requestFactory)
+    }
+    
+    /** Function that configure the manager's `pollTask` then start the polling timer
+     * parameter requestFactory: factory that produces the request for the poll task
+     */
+    private func configurePolling(withRequestFactory requestFactory: RequestFactory<CurrencyRates>)
+    {
+        self.pollTask = PollAsyncTask(requestFactory: requestFactory,
+                                      completion: {
+                                        self.currencyRates = $0.augmented(with: self.currencyInfoDict)
+                                      },
+                                      interval: 1)
+        self.pollTask?.start()
+    }
+}
